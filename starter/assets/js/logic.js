81f52b5744d32377d2e3b01926a75c27e4fee51a
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-01-12 19:04:35 +0000

    questions array

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,1 +1,12 @@
 //Main javascript
+
+const time = [];
+const startScreen = [];
+const start = [];
+const questions = [];
+const questionTitle = [];
+const choices = [];
+const endScreen = [];
+
+
+
--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,3 +1,36 @@
 //Questions array
+const quizQuestions = [
+    {
+        question: "Commonly used data types DO NOT include:",
+        options: ["strings", "booleans", "alerts", "numbers"],
+        answer: "",
+    },
 
+    {
+        question: "The condition in an if / else statement is enclosed within ____.",
+        options: ["quotes", "curly brackets", "parentheses", "square brackets"],
+        answer: "",
+    },
+
+    {
+        question: "Arrays in JavaScript can be used to store ____.",
+        options: ["numbers and strings",
+        "other arrays",
+        "booleans",
+        "all of the above"],
+        answer: "",
+    },
+
+    {
+        question: "String values must be enclosed within ____ when being assigned to variables.",
+        options: ["commas", "curly brackets", "quotes", "parentheses"],
+        answer: "",
+    },
+
+    {
+        question: "A very useful tool used during development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "terminal / bash", "for loops", "console.log"],
+        answer: "",
+    },
+]
 //Answers array
