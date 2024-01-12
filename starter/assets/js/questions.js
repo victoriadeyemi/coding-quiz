ac9c93396552d17c3cd1de99ab7b2b5cd1003a35
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-01-12 19:09:42 +0000

    answer array

--- starter/assets/js/questions.js
+++ starter/assets/js/questions.js
@@ -1,15 +1,15 @@
-//Questions array
+//Questions array and answer array
 const quizQuestions = [
     {
         question: "Commonly used data types DO NOT include:",
         options: ["strings", "booleans", "alerts", "numbers"],
-        answer: "",
+        answer: "alerts",
     },
 
     {
         question: "The condition in an if / else statement is enclosed within ____.",
         options: ["quotes", "curly brackets", "parentheses", "square brackets"],
-        answer: "",
+        answer: "parentheses",
     },
 
     {
@@ -18,19 +18,18 @@ const quizQuestions = [
         "other arrays",
         "booleans",
         "all of the above"],
-        answer: "",
+        answer: "all of the above",
     },
 
     {
         question: "String values must be enclosed within ____ when being assigned to variables.",
         options: ["commas", "curly brackets", "quotes", "parentheses"],
-        answer: "",
+        answer: "quotes",
     },
 
     {
         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
         options: ["JavaScript", "terminal / bash", "for loops", "console.log"],
-        answer: "",
+        answer: "console.log",
     },
 ]
-//Answers array
