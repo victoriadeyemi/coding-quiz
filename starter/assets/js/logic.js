fc375e2e745bf72e6941a83bca47b988b1807efe
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-01-12 20:15:07 +0000

    Completed const variable for each element

--- starter/assets/js/logic.js
+++ starter/assets/js/logic.js
@@ -1,19 +1,24 @@
 //Main javascript
+//const for each id
+const time = document.getElementById("time");
+const startScreen = document.getElementById("start-screen");
+const start =  document.getElementById("start");
+const questions = document.getElementById("questions");
+const questionTitle = document.getElementById("question-title");
+const choices = document.getElementById("choices");
+const endScreen = document.getElementById("end-screen");
+const finalScore = document.getElementById("final-score");
+const initials = document.getElementById("initials");
+const submit = document.getElementById("submit");
+const feedback = document.getElementById("feedback");
 
-const time = [];
-const startScreen = [];
-const start = [];
-const questions = [];
-const questionTitle = [];
-const choices = [];
-const endScreen = [];
-const finalScore = [];
-const initials = [];
-const submit = [];
-const feedback = [];
+//Sound effects
+const correctTune = new Audio("./assets/sfx/correct.wav");
+const incorrectTune = new Audio("./assets/sfx/incorrect.wav");
+//console.log(correctTune)
 
 
 
-
-
-
+//When answer is clicked, the next question appears- use const questions, questiontitle, choices
+//If the answer clicked was incorrect then subtract time from the clock
+//run a timer function
