7e6580a5d9265be86c309e3151fea725bdb2c449
Victoria Adeyemi <151677799+victoriadeyemi@users.noreply.github.com>
2024-01-23 12:59:30 +0000

    function saveScore

--- starter/assets/js/scores.js
+++ starter/assets/js/scores.js
@@ -7,5 +7,18 @@ function getScores() {
     }
     return scores;
 }
-
+function saveScore() {
+    var initials = initials.value.trim();
+    if (initials !== "") {
+        var highscores =
+            JSON.parse(window.localStorage.getItem("highscores")) || [];
+        var newScore = {
+            score: timeLeft,
+            initials: initials
+        };
+        highscores.push(newScore);
+        window.localStorage.setItem("highscores", JSON.stringify(highscores));
+        window.location.href = "highscores.html";
+    }
+}
 getScores();
